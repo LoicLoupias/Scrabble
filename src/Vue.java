1f85b3e9466ced1ab63d0d44b01f643ac688ec5f
Fabien Rols <frols@pcalb-mm0000.univ-jfc.fr>
2020-03-11 15:29:51 +0100

    fin 11/03

--- src/Vue.java
+++ src/Vue.java
@@ -1,4 +1,19 @@
+import java.awt.BorderLayout;
+import java.awt.Frame;
 
-public class Vue {
+public class Vue extends Frame {
+	
+	public static void main(String[] args) {
+		new Vue();
+	}
+	
+	public Vue () {
+		Modele modl = new Modele();
+		Controleur ctrl = new Controleur();
+		
+		this.setLayout(new BorderLayout());
+		
+		
+	}
 
 }
