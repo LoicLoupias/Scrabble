233c52459177182b04d6f129fe4f517ba3ae8736
Thomas Maury <7rchim@gmail.com>
2020-03-31 18:41:03 +0200

    Bouton

--- src/Plateau_de_jeu.java
+++ src/Plateau_de_jeu.java
@@ -1,37 +1,34 @@
+import java.awt.Button;
 import java.awt.Canvas;
+import java.awt.Checkbox;
+import java.awt.CheckboxGroup;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
+import java.awt.Panel;
 import java.util.Observable;
 import java.util.Observer;
 
 @SuppressWarnings({ "deprecation", "serial" })
-public class Plateau_de_jeu extends Canvas implements Observer{
+public class Plateau_de_jeu extends Panel implements Observer{
+	
+	Button[] boutonTab;
 	
 	public Plateau_de_jeu(Modele m) {
 		super();
-		Color vert = new Color(0, 255, 100, 100);
 		this.setPreferredSize(new Dimension(640,360));
+		this.setLayout(new FlowLayout());
 		
-		m.addObserver(this);
-
-		this.setBackground(vert);
-
-	}
-	
-	public void paint(Graphics g) {
-		int x1=this.getWidth()/15;
-		int y1=this.getHeight()/15;
-		int x=0;
-		int y=0;
+		boutonTab = new Button[15];
 		for (int i=0; i<15; i++) {
-			g.drawLine(x,0,x,this.getHeight());
-			g.drawLine(0, y, this.getWidth(),y);
-			x = x + x1;
-			y = y + y1;
+			boutonTab[i] = new Button("");
+			boutonTab[i].setBackground(Color.gray);
+			boutonTab[i].setBounds(50,100,80,30);  
+			add(boutonTab[i]);
 		}
-	}
+		}
+		
 
 	@Override
 	public void update(Observable arg0, Object arg1) {
